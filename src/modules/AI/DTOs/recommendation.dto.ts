5a4be0b0c7ecdf1306fc194904959dd3a1712ccf
lunarist2406 <lyvanmy357@gmail.com>
2026-03-23 21:21:18 +0700

    update

--- src/modules/AI/DTOs/recommendation.dto.ts
+++ src/modules/AI/DTOs/recommendation.dto.ts
@@ -1,9 +1,11 @@
+import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsInt, Min, Max } from 'class-validator';
 
 export class RecommendationDto {
   @ApiProperty({ description: 'Số lượng gợi ý trả về (mặc định 10, tối đa 50)', required: false, example: 10 })
   @IsOptional()
+  @Type(() => Number)   // 👈 thêm dòng này
   @IsInt()
   @Min(1)
   @Max(50)
